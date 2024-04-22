95405fe702f9c206d7e7eb7b5614ffa7a8954db5
tyrrminal <mark@tyrrminal.dev>
2024-04-22 10:31:52 -0400

    refactor

--- lib/Mojolicious/Plugin/Authentication/OIDC.pm
+++ lib/Mojolicious/Plugin/Authentication/OIDC.pm
@@ -4,18 +4,70 @@ use warnings;
 
 # ABSTRACT: OpenID Connect implementation integrated into Mojolicious
 
+=encoding UTF-8
+
+=head1 NAME
+
+Mojolicious::Plugin::Authentication::OIDC - OpenID Connect implementation 
+integrated into Mojolicious
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+Mojolicious plugin for L<OpenID Connect|https://openid.net/developers/how-connect-works/>
+authentication.
+
+=cut
+
 use Mojo::Base 'Mojolicious::Plugin';
+
+use Mojo::UserAgent;
+use Readonly;
 
 use experimental qw(signatures);
 
-sub register($self, $app, $conf) {
-  $conf = ref($conf) eq 'HashRef' ? $conf : {};
-  my %conf = @{$conf}[qw(
-    client_id client_secret scope response_type auth_endpoint token_endpoint grant_type public_key
-    oidc_redirect_path oidc_login_path oidc_success_path
-  )];
+Readonly::Array my @REQUIRED_PARAMS => qw(
+  client_secret
+  well_known_url public_key
+  login_path success_path
+);
+Readonly::Hash  my %DEFAULT_PARAMS  => (
+  scope         => 'openid',
+  response_type => 'code',
+  grant_type    => 'authentication_code',
+  make_routes   => 1,
+);
+#params with runtime defaults: client_id
+#conditionally optional params: redirect_path
 
-  push($app->routes->namespaces->@*, 'Mojolicious::Plugin::Authentication::OIDC::Controller');
+=head1 METHODS
+
+L<Mojolicious::Plugin::Authentication::OIDC> inherits all methods from
+L<Mojolicious::Plugin> and imeplements the following new ones
+
+=head2 register( \%params )
+
+Register plugin in L<Mojolicious> application.
+
+=cut
+
+sub register($self, $app, $params) {
+  my %conf = (
+    %DEFAULT_PARAMS,
+    client_id => lc($app->moniker)
+  );
+  $params = ref($params) eq 'HashRef' ? $params : {};
+  foreach (@REQUIRED_PARAMS) {
+    die("Param '$_' is required") unless(exists($params->{$_}));
+    $conf{$_} = $params->{$_};
+  }
+  die("Param 'redirect_path' is required when 'make_routes' is enabled") if($conf{make_routes} && !exists($params->{redirect_path}));
+
+  my $ua = Mojo::UserAgent->new();
+  my $resp = $ua->get($conf{well_known_url});
+  $conf{auth_endpoint} = $resp->res->json->{authorization_endpoint};
+  $conf{token_endpoint} = $resp->res->json->{token_endpoint};
 
   $app->helper(
     _oidc_params => sub {
@@ -23,10 +75,41 @@ sub register($self, $app, $conf) {
     }
   );
 
-  if($conf->{install_routes}) {
-    $app->routes->get($conf->{oidc_redirect_path})->to("Auth#oidc_redirect");
-    $app->routes->get($conf->{oidc_login_path})->to('Auth#oidc_login');
+  push($app->routes->namespaces->@*, 'Mojolicious::Plugin::Authentication::OIDC::Controller');
+  if($conf{make_routes}) {
+    $app->routes->get($conf{redirect_path})->to("OpenIDConnect#redirect");
+    $app->routes->get($conf{login_path})->to('OpenIDConnect#login');
   }
 }
 
+=head1 AUTHOR
+
+Mark Tyrrell C<< <mark@tyrrminal.dev> >>
+
+=head1 LICENSE
+
+Copyright (c) 2024 Mark Tyrrell
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+=cut
+
 1;
+
+__END__
--- lib/Mojolicious/Plugin/Authentication/OIDC/Controller/OpenIDConnect.pm
+++ lib/Mojolicious/Plugin/Authentication/OIDC/Controller/OpenIDConnect.pm
@@ -5,6 +5,7 @@ use Mojo::Base 'Mojolicious::Controller';
 
 use Crypt::JWT qw(decode_jwt);
 use Mojo::Parameters;
+use Mojo::UserAgent;
 use Data::Printer;
 
 sub make_app_url($self, $path = '/') {
@@ -14,26 +15,28 @@ sub make_app_url($self, $path = '/') {
   $url->path($path);
 }
 
-sub oidc_redirect($self) {
-  my $url = Mojo::URL->new($self->_oidc_params->{auth_endpoint});
-  $url->query({
+sub redirect($self) {
+  my $idp_url = Mojo::URL->new($self->_oidc_params->{auth_endpoint});
+  $idp_url->query({
     client_id     => $self->_oidc_params->{client_id},
     scope         => $self->_oidc_params->{scope},
     response_type => $self->_oidc_params->{response_type},
-    redirect_uri  => $self->make_app_url($self->_oidc_params->{oidc_login_path}),
+    redirect_uri  => $self->make_app_url($self->_oidc_params->{login_path}),
   });
-  $self->redirect_to($url)
+  $self->redirect_to($idp_url)
 }
 
-sub oidc_login($self) {
+sub login($self) {
   my $code = $self->param('code');
   my $ua   = Mojo::UserAgent->new();
-  my $url  = Mojo::URL->new($self->_oidc_params->{token_endpoint})->userinfo(join(':', $self->_oidc_params->{client_id}, $self->_oidc_params->{client_secret}(1)));
+  my $url  = Mojo::URL->new($self->_oidc_params->{token_endpoint})
+    ->userinfo(join(':', $self->_oidc_params->{client_id}, $self->_oidc_params->{client_secret}(1)));
+    
   my $resp = $ua->post(
     $url => form => {
       grant_type   => $self->_oidc_params->{grant_type},
       code         => $code,
-      redirect_uri => $self->make_app_url($self->_oidc_params->{oidc_login_path}),
+      redirect_uri => $self->make_app_url($self->_oidc_params->{login_path}),
     }
   );
 
@@ -44,7 +47,7 @@ sub oidc_login($self) {
     my $token = $resp->res->json->{access_token};
     my $data  = decode_jwt(token => $token, key => \$self->_oidc_params->{public_key});
     p $data;
-    my $success = Mojo::URL->new($self->_oidc_params->{oidc_success_path});
+    my $success = Mojo::URL->new($self->_oidc_params->{success_path});
     $success->query(token => $token);
     $self->redirect_to($success);
   }
