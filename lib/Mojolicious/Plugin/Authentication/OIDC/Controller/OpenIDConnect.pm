67b9c68ca5dabe8b8647b965229c75b443cc38d4
tyrrminal <mark@tyrrminal.dev>
2024-04-25 13:21:45 -0400

    move JWT handling up to OIDC.pm

--- lib/Mojolicious/Plugin/Authentication/OIDC/Controller/OpenIDConnect.pm
+++ lib/Mojolicious/Plugin/Authentication/OIDC/Controller/OpenIDConnect.pm
@@ -3,12 +3,13 @@ use v5.38;
 
 use Mojo::Base 'Mojolicious::Controller';
 
-use Crypt::JWT qw(decode_jwt);
 use Mojo::Parameters;
 use Mojo::UserAgent;
-use Data::Printer;
+use Syntax::Keyword::Try;
 
-sub make_app_url($self, $path = '/') {
+use experimental qw(signatures);
+
+my sub make_app_url($self, $path = '/') {
   my $url = $self->tx->req->url->to_abs->clone;
   $url->fragment(undef);
   $url->query(Mojo::Parameters->new);
@@ -21,7 +22,7 @@ sub redirect($self) {
     client_id     => $self->_oidc_params->{client_id},
     scope         => $self->_oidc_params->{scope},
     response_type => $self->_oidc_params->{response_type},
-    redirect_uri  => $self->make_app_url($self->_oidc_params->{login_path}),
+    redirect_uri  => make_app_url($self, $self->_oidc_params->{login_path}),
   });
   $self->redirect_to($idp_url)
 }
@@ -30,25 +31,30 @@ sub login($self) {
   my $code = $self->param('code');
   my $ua   = Mojo::UserAgent->new();
   my $url  = Mojo::URL->new($self->_oidc_params->{token_endpoint})
-    ->userinfo(join(':', $self->_oidc_params->{client_id}, $self->_oidc_params->{client_secret}(1)));
+    ->userinfo(join(':', $self->_oidc_params->{client_id}, $self->_oidc_params->{client_secret}));
     
   my $resp = $ua->post(
     $url => form => {
       grant_type   => $self->_oidc_params->{grant_type},
       code         => $code,
-      redirect_uri => $self->make_app_url($self->_oidc_params->{login_path}),
+      redirect_uri => make_app_url($self, $self->_oidc_params->{login_path}),
     }
   );
 
   if($resp->res->json->{error}) {
-    p $resp->res->json;
-    return $self->render(openapi => $resp->res->json)
+    return $self->_oidc_params->{on_error}->($self, $resp->res->json);
   } else {
-    my $token = $resp->res->json->{access_token};
-    my $data  = decode_jwt(token => $token, key => \$self->_oidc_params->{public_key});
-    p $data;
-    my $success = Mojo::URL->new($self->_oidc_params->{success_path});
-    $success->query(token => $token);
-    $self->redirect_to($success);
+    try {
+      my $token = $resp->res->json->{access_token};
+      # Decode the token; if it fails, because the key is wrong, or the token
+      # is invalid or has been re-encrypted, then we throw it away and call
+      # error handler
+      $self->_oidc_token($token);
+      return $self->_oidc_params->{on_success}->($self, $token);
+    } catch($e) {
+      $self->log->error($e);
+      $self->_oidc_params->{on_error}->($self, $resp->res->json);
+    }
   }
 }
+
